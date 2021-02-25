ab93fba116200122c45c82976916144d284b22b0
mvaisakh <vaisakhmurali@gmail.com>
2021-02-25 06:38:12 +0530

    Import ARM64 GCC 20210225
    Build completed on: Thu Feb 25 06:38:12 IST 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/gcc-arm64/bin/../libexec/gcc/aarch64-elf/11.0.0/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: single
    Supported LTO compression algorithms: zlib
    gcc version 11.0.0 20210224 (experimental) (Eva GCC)
    
    Signed-off-by: mvaisakh <vaisakhmurali@gmail.com>

--- lib/gcc/aarch64-elf/11.0.0/plugin/include/plugin-version.h
+++ lib/gcc/aarch64-elf/11.0.0/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "11.0.0";
-static char datestamp[] = "20210220";
+static char datestamp[] = "20210224";
 static char devphase[] = "experimental";
 static char revision[] = "";
 
