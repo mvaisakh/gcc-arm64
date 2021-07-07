ef70be16d5b55f36d00ff94a6ff487f9efdf0312
mvaisakh <mvaisakh@statixos.com>
2021-07-07 10:20:25 +0000

    Import ARM64 GCC 20210707
    Build completed on: Wed Jul  7 10:20:25 AM UTC 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/__w/gcc-build/gcc-arm64/bin/../libexec/gcc/aarch64-elf/12.0.0/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --prefix=/__w/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib zstd
    gcc version 12.0.0 20210707 (experimental) (Eva GCC)
    
    LLD: LLD 13.0.0 (https://github.com/llvm/llvm-project 0c1a7730f5372e862150f313df4d9c44757352f4) (compatible with GNU linkers)
    Signed-off-by: mvaisakh <mvaisakh@statixos.com>

--- lib/gcc/aarch64-elf/12.0.0/plugin/include/configargs.h
+++ lib/gcc/aarch64-elf/12.0.0/plugin/include/configargs.h
@@ -1,6 +1,6 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf --prefix=/__w/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
-static const char thread_model[] = "single";
+static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf --prefix=/__w/gcc-build/gcc-build/../gcc-arm64 --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot";
+static const char thread_model[] = "posix";
 
 static const struct {
   const char *name, *value;
